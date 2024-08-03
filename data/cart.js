dce77bf58bc480fb4ad9005de41b33187d3fc52a
Aashish Pandey <pandey786aashish@gmail.com>
2024-08-03 16:41:27 +0530

    in this commit save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,6 @@
-export  let cart=[{
+export  let cart=JSON.parse(localStorage.getItem('cart'));
+if(!cart){
+  cart=[{
     product_id:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity:2
     },
@@ -7,7 +9,11 @@ export  let cart=[{
         quantity:1
     }
 ];
+}
 
+function saveToStorage(){
+  localStorage.setItem('cart',JSON.stringify(cart))
+}
 export function addtocart(product_id){
     let matchingItem;
   
@@ -25,6 +31,7 @@ export function addtocart(product_id){
         quantity:1
       });
     }
+    saveToStorage();
   }
   
  export function removeFromCart(productId){
@@ -36,4 +43,6 @@ export function addtocart(product_id){
     }
    }) 
    cart=newCart;
+
+   saveToStorage();
   }
